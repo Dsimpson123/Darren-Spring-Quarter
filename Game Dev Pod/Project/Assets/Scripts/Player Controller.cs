8ca30769b66f78fed4a3eb30d1494c987cb5c4d6
Dsimpson123 <darren.simpson@hgs.hiddengeniusproject.org>
2026-04-23 17:43:17 -0700

    GameDevApril23

--- Game Dev Pod/Project/Assets/Scripts/Player Controller.cs
+++ Game Dev Pod/Project/Assets/Scripts/Player Controller.cs
@@ -1,16 +1,27 @@
-using UnityEngine;
+ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField, Tooltip(" A reference to the player's rigid body component. Used to apply the movement force")]
+    private Rigidbody rb;
+
+    [SerializeField, Tooltip(" A multiplier for how fast the player moves")]
+    private float moveSpeed = 7f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+
+        float xInput = Input.GetAxis("Horizontal");
+
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(xInput, 0, zInput);
+
+        rb.AddForce(moveDir * moveSpeed * Time.deltaTime);
     }
 }
